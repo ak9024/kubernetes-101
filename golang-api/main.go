c6dd2860b22baab4f22ec81f779d706f5d404f0d
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-12-26 18:03:01 +0800

    remove hostPort

--- golang-api/main.go
+++ golang-api/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+)
+
+type (
+	Response struct {
+		Env interface{} `json:"env"`
+	}
 )
 
 func main() {
 	app := http.NewServeMux()
+
+	app.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		env := Response{
+			Env: os.Getenv("MESSAGE"),
+		}
+		byteEnv, _ := json.Marshal(env)
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(byteEnv)
+	})
 
 	app.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
